91ee4fe24bdf2b5f2454c3d0bcbf3806bb1620db
Alena Kurepina <alenarepina96@gmail.com>
2026-03-30 21:43:40 +0200

    Base64 done

--- main.mjs
+++ main.mjs
@@ -40,7 +40,14 @@ function decodeBrailleToText(bits) {
 
   return result;
 }
-
-
 const base64String = decodeBrailleToText(bitString);
+
+
+const base64Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+const base64Binary = base64String
+  .split("")
+  .map(character => base64Characters.indexOf(character).toString(2).padStart(6, "0"))
+  .join("");
+
 console.log(base64String);
+console.log(base64Binary);
