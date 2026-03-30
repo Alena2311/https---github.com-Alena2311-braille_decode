5511771e728d61471745c77dc27f32211a1d7841
Alena Kurepina <alenarepina96@gmail.com>
2026-03-30 21:30:14 +0200

    Braille part done

--- main.mjs
+++ main.mjs
@@ -1,1 +1,46 @@
-const splitBitChunks = bitString.match(/.{1,6}/g)
+import { readFileSync } from "fs";
+import { brailleAlphabet } from "./brailleAlphabet.mjs";
+
+const input = readFileSync("./input.txt", "utf-8");
+const bitString = input.replace(/\s+/g, "");
+
+
+function decodeBrailleToText(bits) {
+  const chunks = bits.match(/.{6}/g);
+  let result = "";
+  let capitalizedFollows = false;
+  let numberFollows = false;
+
+  for (const chunk of chunks) {
+    const character = brailleAlphabet[chunk];
+
+    if (character === "CAP") {
+      capitalizedFollows = true;
+      continue;
+    }
+
+    if (character === "NUM") {
+      numberFollows = true;
+      continue;
+    }
+
+    if (character === " ") {
+      numberFollows = false;
+      result += " ";
+      continue;
+    }
+
+    if (capitalizedFollows) {
+      result += character.toUpperCase();
+      capitalizedFollows = false;
+    } else {
+      result += character;
+    }
+  }
+
+  return result;
+}
+
+
+const base64String = decodeBrailleToText(bitString);
+console.log(base64String);
